5e056f7336247a8dc44d475e34245adedf7f53a3
Sławosz <slawosz@gmail.com>
2015-05-18 20:31:50 +0200

    old, old commit

--- test/param_protection_test.rb
+++ test/param_protection_test.rb
@@ -1,10 +1,10 @@
 require 'helper'
 require 'action_controller'
 
-class BaseController < ActionController::Metal
+class BaseController < ActionController::Base
 
   def index
-    
+    render nil
   end
   
 end
@@ -13,16 +13,16 @@ class ParamsProtectedController < BaseController
   params_protected :framework
 end
 
-class TestParamProtection < ActiveSupport::TestCase
+class ParamsProtectedControllerTest < ActionController::TestCase
   
   test "should remove protected parameter" do
-    request = ActionDispatch::TestRequestWithParams.new
-    request.params = {:user => 'slawosz',:framework => 'rails',:lang => 'ruby'}
+    with_routing do |set|
+      set.draw { match ':controller/:action' }
+      p @request.inspect
+      get :index, {:user => 'slawosz',:framework => 'rails',:lang => 'ruby'}
 
-    controller = ParamsProtectedController.new
-    controller.dispatch('index', request)
-
-    assert_equal ({:user => 'slawosz',:lang => 'ruby'}), controller.params
+      assert_equal ({:user => 'slawosz',:lang => 'ruby'}), @controller.params
+    end
   end
 end
 
@@ -30,7 +30,7 @@ class ParamsAccessiblecontroller < BaseController
   params_accessible :framework
 end
 
-class TestParamAccessible < ActiveSupport::TestCase
+class ParamsAccessiblecontrollerTest < ActiveSupport::TestCase
   
   test "should remove protected parameter" do
     request = ActionDispatch::TestRequestWithParams.new
